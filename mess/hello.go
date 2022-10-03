728a7cccd206f78076936f7e05ed2cfad6e66c65
Robert Shi <mingyunyuansu@163.com>
2022-10-03 21:08:47 +0800

    before crawler

--- mess/hello.go
+++ mess/hello.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (rot *rot13Reader) Read(b []byte) (int, error) {
+	n, err := rot.r.Read(b)
+	for i := 0; i < n; i++ {
+		if b[i] >= 'A' && b[i] <= 'Z' {
+			b[i] = (b[i]-'A'+13)%26 + 'A'
+		} else if b[i] >= 'a' && b[i] <= 'z' {
+			b[i] = (b[i]-'a'+13)%26 + 'a'
+		}
+	}
+	return n, err
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
 }
